1c19ed8cb0fd5546c48b1a7a9866ba634940b52b
saksham-s1ngh <saksham249singh@gmail.com>
2024-03-06 19:17:28 -0500

    added 'delegate' and initialised it as 'this' in constructor.

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -1,19 +1,27 @@
 package org.example;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 
 public class MyStack<E> extends ArrayList<E> {
 
+    private ArrayList<E> delegate;
+
+    public MyStack() {
+        delegate = this;
+    }
+
     public void push(E e) {
-        add(e);
+        delegate.add(e);
     }
 
     public E pop() {
-        if (isEmpty()) throw new EmptyStackException();
+        if (delegate.isEmpty()) throw new EmptyStackException();
         E e = get(size() - 1);
         remove(size() -1 );
         return e;
+
     }
 
 }
